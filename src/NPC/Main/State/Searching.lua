1a4f37d95b56b919b4e1bcfc7b647079840341c1
vincent <125838532+dogo8me@users.noreply.github.com>
2025-07-03 21:48:26 -0400

    Update Searching.lua

--- src/NPC/Main/State/Searching.lua
+++ src/NPC/Main/State/Searching.lua
@@ -4,15 +4,15 @@ local character = script.Parent.Parent.Parent.Parent
 local humanoidRootPart = character.HumanoidRootPart
 
 function SearchState.search(targetPlayer: Player, lastKnownLocation: Vector3)
-	local nodesToReset = main.getNodesInRadius(lastKnownLocation, 50)
+	local nodesToReset = main.getNodesInRadius(lastKnownLocation, 50, true)
 
 	for _, node in nodesToReset do
 		node:SetAttribute("IsSearched", false)
 		node.BrickColor = BrickColor.new("New Yeller")
 	end
 
-	local nodesInRadius = main.getNodesInRadius(lastKnownLocation, 35)
-
+	local nodesInRadius = main.getNodesInRadius(lastKnownLocation, 35, true)
+	
 	local lastClosestNode = nil
 
 	main.pathToNode(lastKnownLocation, nil, 2)
@@ -20,6 +20,7 @@ function SearchState.search(targetPlayer: Player, lastKnownLocation: Vector3)
 	while task.wait() do
 		local startNode = lastClosestNode or nodesInRadius[math.random(1, #nodesInRadius)]
 		lastClosestNode = main.getClosestNode(humanoidRootPart, false, nodesInRadius)
+		if not lastClosestNode then print(nodesInRadius) end
 		lastClosestNode.BrickColor = BrickColor.new("Bright red")
 		main.pathToNode(startNode.Position, function(pathStatus)
 			if pathStatus == Enum.PathStatus.Success then
